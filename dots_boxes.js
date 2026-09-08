8620769b9b3a7c703ef3d9b0752e992af38acf49
leoamirstar-cmd <leoamirstar@gmail.com>
2026-09-08 07:23:16 +0330

    Create dots_boxes.js

---
+++ dots_boxes.js
@@ -0,0 +1,5 @@
+function initDotsAndBoxes() {
+  const container = document.getElementById('board-container');
+  container.innerHTML = '<p style="color: #a6e3a1;">منطق بازی نقطه و خط به‌زودی بارگذاری می‌شود...</p>';
+}
+
